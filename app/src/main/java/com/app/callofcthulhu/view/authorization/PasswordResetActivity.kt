d2dc57db75d681348544ca13e5503a20134dd5a0
Karol Krawczyk <krawiecc11@gmail.com>
2024-01-15 16:43:56 +0100

    Zmiana struktury, rozdział zapisu do repozytorium

--- app/src/main/java/com/app/callofcthulhu/view/authorization/PasswordResetActivity.kt
+++ app/src/main/java/com/app/callofcthulhu/view/authorization/PasswordResetActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.app.callofcthulhu.R
 import com.google.android.material.textfield.TextInputEditText
@@ -18,22 +19,35 @@ class PasswordResetActivity : AppCompatActivity() {
 
         val editTextEmail = findViewById<TextInputEditText>(R.id.resetEmail)
         val buttonReset = findViewById<Button>(R.id.btn_reset)
+        val buttonBack = findViewById<TextView>(R.id.btn_back_login)
 
-
-        buttonReset.setOnClickListener{
+        buttonReset.setOnClickListener {
             val auth = FirebaseAuth.getInstance()
-            val email = editTextEmail.text.toString()
-            auth.sendPasswordResetEmail(email)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(baseContext, "Wysłano maila z resetem hasła", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(applicationContext, LoginActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        Toast.makeText(baseContext, "Błąd z wysłaniem maila", Toast.LENGTH_SHORT).show()
+            val email = editTextEmail.text.toString().trim()
+
+            if (email.isEmpty()) {
+                // Wyświetlamy komunikat, że pole email jest puste
+                Toast.makeText(baseContext, "Pole email nie może być puste", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(baseContext, "Wysłano maila z resetem hasła", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(applicationContext, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(baseContext, "Niepoprawny adres email", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
+            }
+        }
+
+
+        buttonBack.setOnClickListener{
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
 
